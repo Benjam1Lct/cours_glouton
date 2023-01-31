4e94de577dedadc58e715c98e22ed426b96eaca9
PearlClone <benjaminlecomte37@gmail.com>
2023-01-31 11:39:34 +0100

    update

--- exercice_6.py
+++ exercice_6.py
@@ -4,24 +4,34 @@ fin = [13,17,11,16,12]
 horaire = {'A':(8,13), 'B':(12,17), 'C':(9,11), 'D':(14,16), 'E':(11,12)}
 
 def prochaine(debut, fin, h):
-    min = None
-    debut_stock = 0
-    fin_stock = 0
-    for i in range(len(debut)-1):
+    opti = None
+    for i in range(len(debut)):
         if debut[i] >= h:
-            if min is None:
-                min = (fin[i] - debut[i])
-                debut_stock = debut[i]
-                fin_stock = fin[i]
-            elif (fin[i] - debut[i]) < min:
-                min = (fin[i] - debut[i])
-                debut_stock = debut[i]
-                fin_stock = fin[i]
-    return str(debut_stock) + 'h-' + str(fin_stock) + 'h'
+            if opti == None:
+                opti = [debut[i],fin[i]]
+            elif opti[1] - opti[0] > fin[i] - debut[i] and fin[i] < opti[1]:
+                opti = [debut[i],fin[i]]
+        else:
+            pass
+
+    return opti
 
 
 def selection(debut, fin):
-    prochaine(debut, fin, )
-    while
+
+    h = 8
+
+    max_debut = 0
+    lst_act = []
+
+    for i in range(len(debut)-1):
+        if debut[i] > max_debut:
+            max_debut = debut[i]
+
+    while h < max_debut:
+        lst_act.append(prochaine(debut,fin,h))
+        h = prochaine(debut,fin,h)[1]
+ 
+    return lst_act
 
 print(prochaine(debut, fin, 8))
